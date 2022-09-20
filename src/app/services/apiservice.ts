9d59cd76c3a6c93991ad2fbc268999b89ea61e37
balunandam <35144614+balunandam@users.noreply.github.com>
2022-09-20 18:28:00 +0530

    1 file modified

--- src/app/services/apiservice.ts
+++ src/app/services/apiservice.ts
@@ -8,13 +8,6 @@ import { catchError, map } from 'rxjs/operators';
 })
 export class ApiService {
   constructor(private http: HttpClient) {}
-  // getWeather(location) {
-  //   debugger;
-  //   return this.http.get(
-  //     `https://api.openweathermap.org/data/2.5/weather?APPID=eb03b1f5e5afb5f4a4edb40c1ef2f534&q=${location}`
-  //   );
-  // }
-
   public getWeather(data): Observable<any> {
     const datas = data;
     return this.http
